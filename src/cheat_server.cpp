a3620ed4de4fde14f57e542fc550344ab234eb52
Liu Wei <liuwei@4paradigm.com>
2017-04-28 19:42:09 +0800

    add mysqllib

--- src/cheat_server.cpp
+++ src/cheat_server.cpp
@@ -2,6 +2,7 @@
 #include "CheatWorker.h"
 #include <signal.h>
 #include <cpp_redis/cpp_redis>
+#include <mysql_devapi.h>
 volatile std::atomic<bool> should_exit = ATOMIC_VAR_INIT(false);
 void calc(int i) {
     std::cout << i << "th task, the answer is: " << i * i << std::endl;
@@ -13,7 +14,6 @@ void sigint_handler(int) {
 
 
 int main(int argc, char *argv[]) {
-    std::cout << "Hello, World!" << std::endl;
     cpp_redis::redis_subscriber client;
     client.connect();
     client.subscribe("cheat", [](const std::string& chan, const std::string& msg) {
