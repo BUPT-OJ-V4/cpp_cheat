e96e377967071bb7024968f5b37962f35b776738
2013211450 <bupt2013211450@gmail.com>
2017-10-18 00:18:19 +0800

    rm makefile

--- build.sh
+++ build.sh
@@ -6,12 +6,15 @@
 #########################################################################
 #!/bin/bash
 
+pkg_name = $(shell echo CheatServer-`date +%Y%m%d-%H%M%S`.tar.gz)
+echo ${pkg_name}
+
+if [ -d ./build ]; then
 rm -rf build
+fi
+
 mkdir build
 cd build && cmake ..
 make clean
 make -j4
 
-mkdir release
-mkdir release/logs
-mv bin release/
